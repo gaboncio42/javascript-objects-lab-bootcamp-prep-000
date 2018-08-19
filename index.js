43e3ce5c788b15f0b2073d1a8149b279e394c67d
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-19 22:33:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(objec, key, value) {
-  
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object.key
 }
