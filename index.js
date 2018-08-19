3e0157d027aa8f2bac26351c8b2d5110b9e548dc
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-19 22:26:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
   var obj = {prop: 1};
-  return Object.assign({}, object, {[prop]:'2'});
+  return Object.assign({}, object, {['prop']:'2'});
 }
